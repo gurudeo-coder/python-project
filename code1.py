e38e29846fc46dbbd96df8aafa89d26289fb2e39
gurudeo padwe <85779121+gurudeo-coder@users.noreply.github.com>
2021-06-13 09:28:47 +0530

    Create code1.py

---
+++ code1.py
@@ -0,0 +1,6 @@
+name =input("enter the name :")
+colour=input("enter the colour name:")
+city=input("enter the name of city")
+college=input("enter the name of college:")
+
+print('my name is',name,'i love',colour,'coulour I leave in ',city,'my college name is',college,'.')
